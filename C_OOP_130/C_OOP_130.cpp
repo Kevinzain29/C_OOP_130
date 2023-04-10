ed9c0e779712eb207e648189b608204c036123e8
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-04-10 10:44:00 +0700

    Add project files.

---
+++ C_OOP_130/C_OOP_130.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+	string nim;
+	string nama;
+public: void cetak() {
+	cout << "NIM = " << NIM;
+	cout << "\nnama = " << nama;
+	}
+};
+
